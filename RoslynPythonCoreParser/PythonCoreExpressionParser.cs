84cd9c95ae272cfb5891104d856c9240fc9d9016
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-12-15 16:36:39 +0100

    Empty expression rules added.

--- RoslynPythonCoreParser/PythonCoreExpressionParser.cs
+++ RoslynPythonCoreParser/PythonCoreExpressionParser.cs
@@ -2,5 +2,160 @@
 
 public partial class PythonCoreParser
 {
+
+    public ExprNode ParseNamedExpr()
+    {
+        throw new NotImplementedException();
+    }
+
+    public ExprNode ParseTest()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseLambdaDef(bool isConditional)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseOrTest()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseAndTest()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseNotTest()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseComparison()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseStarExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseXorExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseAndExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseShiftExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseArithExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseTerm()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseFactor()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParsePower()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseAtomExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseAtom()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseTestListComp()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseTrailer()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseSubscriptList()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseExprList()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseTestList()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseDictorSetMaker()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseArgList()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseArgument()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseCompIter()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseSyncCompFor()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseCompFor()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseCompIf()
+    {
+        throw new NotImplementedException();
+    }
+    
+    public ExprNode ParseYieldExpr()
+    {
+        throw new NotImplementedException();
+    }
     
 }
