c1a422843c8940ed36515c3f17f3e56f743cebbb
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-12-15 14:54:13 +0100

    More Expr nodes.

--- RoslynPythonCoreParser/SyntaxNode.cs
+++ RoslynPythonCoreParser/SyntaxNode.cs
@@ -17,9 +17,44 @@ public sealed record LambdaExpr(uint Start, uint End, Token Symbol1, ExprNode Le
 public sealed record OrTestExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
     : ExprNode(Start, End);
 
+public sealed record AndTestExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
 
+public sealed record NotTestExprNode(uint Start, uint End, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
 
+public sealed record CompareLessExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
 
+public sealed record CompareGreaterExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareEqualExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareGreaterEqualExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareLessEqualExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareNotEqualExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareInExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareNotInExprNode(uint Start, uint End, ExprNode Left, Token Symbol1, Token Symbol2, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareIsExprNode(uint Start, uint End, ExprNode Left, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record CompareIsNotExprNode(uint Start, uint End, ExprNode Left, Token Symbol1, Token Symbol2, ExprNode Right)
+    : ExprNode(Start, End);
+
+public sealed record StarExprNode(uint Start, uint End, Token Symbol, ExprNode Right)
+    : ExprNode(Start, End);
 
 
 
