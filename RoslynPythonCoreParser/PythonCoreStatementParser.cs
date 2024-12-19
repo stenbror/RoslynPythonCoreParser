d735d00f60d94e5c43994877ae88e7ea42ef3878
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-12-19 20:49:28 +0100

    Template for stmt rules.

--- RoslynPythonCoreParser/PythonCoreStatementParser.cs
+++ RoslynPythonCoreParser/PythonCoreStatementParser.cs
@@ -8,8 +8,162 @@ public partial class PythonCoreParser
         throw new NotImplementedException();
     }
     
+    private StmtNode ParseStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseSimpleStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseSmallStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseExprStmt()
+    {
+        throw new NotImplementedException();
+    }
     
     private StmtNode ParseTestListStarExpr()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseDelStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParsePassStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseFlowStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseBreakStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseContinueStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseReturnStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseRaiseStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseImportStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseImportNameStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseImportFromStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseImportAsNameStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseDottedAsNameStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseImportAsNamesStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseDottedAsNamesStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseDottedNameStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseGlobalStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseNonlocalStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseAssertStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseCompoundStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseIfStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseWhileStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseForStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseTryStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseWithStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseWithItemStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseExceptClauseStmt()
+    {
+        throw new NotImplementedException();
+    }
+    
+    private StmtNode ParseSuiteStmt()
     {
         throw new NotImplementedException();
     }
