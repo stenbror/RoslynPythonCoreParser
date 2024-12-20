92324f331244f83ce01687aa357d0936858373bf
Richard Magnor Stenbro <stenbror@hotmail.com>
2024-12-20 22:23:11 +0100

    Started on SyntaxError exception for parse errors.

---
+++ RoslynPythonCoreParser/SyntaxError.cs
@@ -0,0 +1,7 @@
+﻿namespace RoslynPythonCoreParser;
+
+public class SyntaxError(uint pos, string text) : Exception
+{
+    public uint Location { get; init; } = pos;
+    public string Text { get; init; } = text;
+}
